5975a30fed2379536849144149ac81b9867a95a1
kheljit <76439921+kheljit@users.noreply.github.com>
2021-02-23 21:02:47 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,99 +1,91 @@
-var mouseEvent = "empty";
-var last_position_of_x, last_position_of_y;
+canvas = document.getElementById('myCanvas');
+ctx = canvas.getContext("2d");
 
-    canvas = document.getElementById('myCanvas');
-    ctx = canvas.getContext("2d");
-    
-    color = "black";
-    width_of_line = 1;
+car1_width = 120;
+car1_height = 70;
+car1_image = "https://i.postimg.cc/9rqYz9HM/car1.png";
+car1_x = 10;
+car1_y = 10;
 
-    canvas.addEventListener("mousedown", my_mousedown);
-    
-    function my_mousedown(e)
-    {
-        //Addictonal Activity start
-        color = document.getElementById("color").value;
-        width_of_line = document.getElementById("width_of_line").value;
-        //Addictonal Activity ends
+car2_width = 120;
+car2_height = 70;
+car2_image = "https://i.postimg.cc/tnnW1Kff/car2.png";
+car2_x = 10;
+car2_y = 100;
 
-        mouseEvent = "mouseDown";
-    }
+background_image = "https://i.postimg.cc/bv5d35nK/racing.jpg";
 
-    canvas.addEventListener("mousemove", my_mousemove);
-    function my_mousemove(e)
-    {
-        current_position_of_mouse_x = e.clientX - canvas.offsetLeft;
-        current_position_of_mouse_y = e.clientY - canvas.offsetTop;
+function add() {
+	background_imgTag = new Image(); //defining a variable with a new image
+	background_imgTag.onload = uploadBackground; // setting a function, onloading this variable
+	background_imgTag.src = background_image;   // load image
 
-        if (mouseEvent == "mouseDown") {
-        ctx.beginPath();
-        ctx.strokeStyle = color;
-        ctx.lineWidth = width_of_line;
-        ctx.moveTo(last_position_of_x, last_position_of_y);
-        ctx.lineTo(current_position_of_mouse_x, current_position_of_mouse_y);
-        ctx.stroke();
-        }
+	car1_imgTag = new Image(); //defining a variable with a new image
+	car1_imgTag.onload = uploadcar1; // setting a function, onloading this variable
+	car1_imgTag.src = car1_image;   // load image
 
-        last_position_of_x = current_position_of_mouse_x; 
-        last_position_of_y = current_position_of_mouse_y;
-    }
+	car2_imgTag = new Image(); //defining a variable with a new image
+	car2_imgTag.onload = uploadcar2; // setting a function, onloading this variable
+	car2_imgTag.src = car2_image;   // load image
+}
 
-    canvas.addEventListener("mouseup", my_mouseup);
-    function my_mouseup(e)
-    {
-        mouseEvent = "mouseUP";
-    }
+function uploadBackground() {
+	ctx.drawImage(background_imgTag, 0, 0, canvas.width, canvas.height);
+}
 
-    canvas.addEventListener("mouseleave", my_mouseleave);
-    function my_mouseleave(e)
-    {
-        mouseEvent = "mouseleave";
-    }
+function uploadcar1() {
+	ctx.drawImage(car1_imgTag, car1_x, car1_y, car1_width, car1_height);
+}
+function uploadcar2() {
+	ctx.drawImage(car2_imgTag, car2_x, car2_y, car2_width, car2_height);
+}
 
-var last_position_of_touch_x, last_position_of_touch_y;
-var width = screen.width;
 
-new_width =  screen.width - 70; 
-new_height = screen.height - 300;
-    if(width < 992)
-    {
-    document.getElementById("myCanvas").width = new_width;
-    document.getElementById("myCanvas").height = new_height;
-    document.body.style.overflow = "hidden";
-    }
-    canvas.addEventListener("touchstart", my_touchstart);
+window.addEventListener("keydown", my_keydown);
 
-function my_touchstart(e) 
+function my_keydown(e)
 {
-    console.log("my_touchstart");
-  //Additional Activity
-  color = document.getElementById("color").value;
-  width_of_line = document.getElementById("width_of_line").value;
-  //End Additional Activity
-         
-    last_position_of_touch_x = e.touches[0].clientX - canvas.offsetLeft;
-    last_position_of_touch_y = e.touches[0].clientY - canvas.offsetTop;
+	keyPressed = e.keyCode;
+	console.log(keyPressed);
+		if(keyPressed == '38')
+		{
+			car1_up();
+			console.log("up arrow key");
+		}
+		if(keyPressed == '40')
+		{
+			car1_down();
+			console.log("down arrow key");
+		}
+		if(keyPressed == '37')
+		{
+			car1_left();
+			console.log("left arrow key");
+		}
+		if(keyPressed == '39')
+		{
+			car1_right();
+			console.log("right arrow key");
+		}
+		if(keyPressed == '87')
+		{
+			car2_up();
+			console.log("key w");
+		}
+		if(keyPressed == '83')
+		{
+			car2_down();
+			console.log("key s");
+		}
+		if(keyPressed == '65')
+		{
+			car2_left();
+			console.log("key a");
+		}
+		if(keyPressed == '68')
+		{
+			car2_right();
+			console.log("key d");
+		}
 }
 
-canvas.addEventListener("touchmove", my_touchmove);
-
-function my_touchmove(e) 
-{
-    current_position_of_touch_x = e.touches[0].clientX - canvas.offsetLeft;
-    current_position_of_touch_y = e.touches[0].clientY - canvas.offsetTop;
-    ctx.beginPath();
-    ctx.strokeStyle = color;
-    ctx.lineWidth = width_of_line;
-    ctx.moveTo(last_position_of_touch_x, last_position_of_touch_y);
-    ctx.lineTo(current_position_of_touch_x, current_position_of_touch_y);
-    ctx.stroke();
-
-    last_position_of_touch_x = current_position_of_touch_x; 
-    last_position_of_touch_y = current_position_of_touch_y;
-   
-}
-
-//Additional Activity
-function clearArea() {
-    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-}
